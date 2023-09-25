f78e01ed00fdaecfe3e551b2bb71f50a1f9ec573
Samrat Bhusal <bhusalsamrat11@gmail.com>
2023-09-25 13:40:17 +0545

    added hero part

---
+++ src/hero/home.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Home.css"
+
+const Home = () => {
+    return (
+      
+        <section className='hero' id='home'>
+          <div className='container f_flex top'>
+            <div className='left top'>
+              <h3>WELCOME TO MY WORLD</h3>
+              <h1>
+                Hi, I’m <span>Samrat Bhusal</span>
+              </h1>
+    </div>
+    </div>
+    </section>
+    )
+}
