75579055fac5ece0a698affe460b3b27499ac6b8
Samrat Bhusal <bhusalsamrat11@gmail.com>
2023-09-26 22:41:15 +0545

    all new changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Contact from './contact/contact';
 import Header from './head/header.js';
 import Home from './hero/home.js';
-import Portfolio_data from './portfolio/portfolio';
 import Resume from './resume/resume';
 
 function App() {
