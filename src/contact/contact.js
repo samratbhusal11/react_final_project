db547d7b21bf441e6445ae02d5e46a880215a403
Samrat Bhusal <bhusalsamrat11@gmail.com>
2023-09-26 21:23:11 +0545

    contacta

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -10,3 +10,28 @@ const Contact = () => {
     message: "",
   });
 
+  const InputEvent = (event) => {
+    const { name, value } = event.target;
+
+    setData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const formSubmit = (event) => {
+    event.preventDefault();
+    alert(`My name is ${data.fullname}. 
+      My phone number is ${data.phone}. 
+      My email address is ${data.email}. 
+      My Subject on  ${data.subject}. 
+      Here is my message I want to say: ${data.message}.`);
+  };
+
+  return (
+    <section className="Contact" id="contact">
+      <div className="container top">
+        <div className="heading text-center">
+          <h4>CONTACT</h4>
+          <h1 className="text-3xl font-semibold">Contact With Me</h1>
+        </div>
