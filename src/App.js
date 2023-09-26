bf1828e0e0953e0c1beacdf037108103908d5150
Samrat Bhusal <bhusalsamrat11@gmail.com>
2023-09-26 13:57:43 +0545

    resume added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Contact from './contact/contact';
 import Header from './head/header.js';
 import Home from './hero/home.js';
 import Portfolio_data from './portfolio/portfolio';
+import Resume from './resume/resume';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
 <Header/> 
 <Home/>
 <Contact/>
+<Resume/>
+
 
 </>
   );
