d3e5e90ced8c1ce99e71b9fab82bcd928e8c57f6
Samrat Bhusal <bhusalsamrat11@gmail.com>
2023-09-26 12:03:53 +0545

    home changes with tailwind css

--- src/hero/home.js
+++ src/hero/home.js
@@ -1,18 +1,24 @@
-import React from "react"
-import "./Home.css"
+import React from "react";
+import "./home.css"; // Import your CSS file for any additional styling
+import hero from "./hero.jpg";
+import { Typewriter } from "react-simple-typewriter";
 
 const Home = () => {
-    return (
-      
-        <section className='hero' id='home'>
-          <div className='container f_flex top'>
-            <div className='left top'>
-              <h3>WELCOME TO MY WORLD</h3>
-              <h1>
-                Hi, I’m <span>Samrat Bhusal</span>
-              </h1>
-    </div>
-    </div>
-    </section>
-    )
-}
+  return (
+    <>
+      <section className="hero bg-gray-100 py-16">
+        <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+          <div className="md:w-1/2">
+            <h1 className="text-4xl md:text-5xl font-bold text-gray-800 mb-4">
+              Hi, I'm <span className="text-pink-500">Samrat Bhusal</span>
+            </h1>
+            
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
