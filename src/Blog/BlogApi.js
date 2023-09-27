a812c7fb195bc8ca7f77d32c996336663e7d1d2d
Samrat Bhusal <bhusalsamrat11@gmail.com>
2023-09-27 22:05:03 +0545

    blog and some changes

---
+++ src/Blog/BlogApi.js
@@ -0,0 +1,44 @@
+import Blog1 from "../pic/blog1.jpg"
+import Blog2 from "../pic/blog2.jpg"
+import Blog3 from "../pic/blog3.jpg"
+
+const BlogApi = [
+
+  {
+    "id": 1,
+    "image": Blog1,
+    "date": "2 May, 2021",
+    "title_one": "Lorem Ipsum Blog Post 1",
+    "desc_one": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac sapien vel odio tristique gravida. Fusce tincidunt venenatis urna, a lacinia ex tincidunt eu. Nullam eget odio sit amet velit interdum malesuada. Sed efficitur euismod justo, sed dapibus velit ullamcorper vel.",
+    "title_two": "Sed Ac Sapien Vel Odio 1",
+    "desc_two": "Suspendisse potenti. Suspendisse potenti. In hac habitasse platea dictumst. Nulla facilisi. Nullam eu ligula sit amet urna tincidunt convallis nec sed orci. In ac ante eu elit fringilla consectetur ut a ligula. Curabitur sit amet volutpat velit.",
+    "title_three": "Nulla Facilisi. Nullam Eu Ligula 1",
+    "desc_three": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam laoreet justo in dolor bibendum, vel posuere velit varius. Ut condimentum nisi in libero congue, non venenatis ligula vestibulum. Vivamus in dolor vel erat venenatis hendrerit."
+  },
+  
+  {
+    "id": 2,
+    "image": Blog2,
+    "date": "28 April, 2021",
+    "title_one": "Lorem Ipsum Blog Post 2",
+    "desc_one": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac sapien vel odio tristique gravida. Fusce tincidunt venenatis urna, a lacinia ex tincidunt eu. Nullam eget odio sit amet velit interdum malesuada. Sed efficitur euismod justo, sed dapibus velit ullamcorper vel.",
+    "title_two": "Sed Ac Sapien Vel Odio 2",
+    "desc_two": "Suspendisse potenti. Suspendisse potenti. In hac habitasse platea dictumst. Nulla facilisi. Nullam eu ligula sit amet urna tincidunt convallis nec sed orci. In ac ante eu elit fringilla consectetur ut a ligula. Curabitur sit amet volutpat velit.",
+    "title_three": "Nulla Facilisi. Nullam Eu Ligula 2",
+    "desc_three": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam laoreet justo in dolor bibendum, vel posuere velit varius. Ut condimentum nisi in libero congue, non venenatis ligula vestibulum. Vivamus in dolor vel erat venenatis hendrerit."
+  },
+  
+  {
+    "id": 3,
+    "image": Blog3,
+    "date": "20 Jun, 2021",
+    "title_one": "Lorem Ipsum Blog Post 3",
+    "desc_one": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac sapien vel odio tristique gravida. Fusce tincidunt venenatis urna, a lacinia ex tincidunt eu. Nullam eget odio sit amet velit interdum malesuada. Sed efficitur euismod justo, sed dapibus velit ullamcorper vel.",
+    "title_two": "Sed Ac Sapien Vel Odio 3",
+    "desc_two": "Suspendisse potenti. Suspendisse potenti. In hac habitasse platea dictumst. Nulla facilisi. Nullam eu ligula sit amet urna tincidunt convallis nec sed orci. In ac ante eu elit fringilla consectetur ut a ligula. Curabitur sit amet volutpat velit.",
+    "title_three": "Nulla Facilisi. Nullam Eu Ligula 3",
+    "desc_three": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam laoreet justo in dolor bibendum, vel posuere velit varius. Ut condimentum nisi in libero congue, non venenatis ligula vestibulum. Vivamus in dolor vel erat venenatis hendrerit."
+  },
+  
+]
+export default BlogApi
